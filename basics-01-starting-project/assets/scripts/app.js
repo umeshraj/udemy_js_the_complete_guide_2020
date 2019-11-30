b745377c6037020e274f95a2d547b0284352a6a2
Umesh <umesh.rajashekar@gmail.com>
2019-11-30 18:26:37 -0500

    convert input from string to number

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,7 +2,7 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-  currentResult = currentResult + userInput.value;
+  currentResult = currentResult + parseInt(userInput.value);
   calculationDescription = "";
   outputResult(currentResult, calculationDescription);
 }
