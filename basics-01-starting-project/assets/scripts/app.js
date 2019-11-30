df005a9166af538a118da2d6d22597abc0c0ecd9
Umesh <umesh.rajashekar@gmail.com>
2019-11-30 11:10:53 -0500

    Example of creating/using function

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert("The result is " + result);
+}
+add(3, 5);
+
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
