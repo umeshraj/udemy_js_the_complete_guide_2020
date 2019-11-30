8be682854e957f680dfb7020fa73f6db9a793aa3
Umesh <umesh.rajashekar@gmail.com>
2019-11-30 09:48:11 -0500

    Template literal

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,6 +2,6 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-let calculationDescription = "(" + defaultResult + " + 10) * 3 / 2 - 1";
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
