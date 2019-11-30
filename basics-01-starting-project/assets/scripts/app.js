fba04e46ee53aa80c22160af693babf0c01fb2a4
Umesh <umesh.rajashekar@gmail.com>
2019-11-30 09:42:43 -0500

    String concat w/ const variable

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,6 +1,7 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-let calculationDescription = "(0 + 10) * 3 / 2 - 1";
+let calculationDescription = "(" + defaultResult + " + 10) * 3 / 2 - 1";
 
 outputResult(currentResult, calculationDescription);
