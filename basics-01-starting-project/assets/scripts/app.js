5925b7185fef538b9d791070d1f8d75affe0bd30
Umesh <umesh.rajashekar@gmail.com>
2019-11-30 18:35:54 -0500

    Move user input access to function

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,8 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
 function add() {
-  const enteredNumber = parseInt(userInput.value);
+  const enteredNumber = getUserNumberInput();
   const calculationDescription = `${currentResult} + ${enteredNumber}`;
   currentResult = currentResult + enteredNumber;
   outputResult(currentResult, calculationDescription);
