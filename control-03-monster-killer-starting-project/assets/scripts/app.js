bf660352a9405a8a55cb8e32731d9307e02e6677
Umesh <umesh.rajashekar@gmail.com>
2019-12-02 11:47:12 -0500

    Adding monster damage

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,5 +1,7 @@
 const ATTACK_VALUE = 10;
-let chosenMaxLife = 100;
+const MONSTER_ATTACK_VALUE = 14;
+
+let chosenMaxLife = 5;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
@@ -8,6 +10,17 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
+
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You lost");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("You have a draw!");
+  }
 }
 
 attackBtn.addEventListener("click", attackHandler);
