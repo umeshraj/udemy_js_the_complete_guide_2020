ee279a63c90b63296ea47323478e80fd6d0203ab
Umesh <umesh.rajashekar@gmail.com>
2019-12-05 06:16:58 -0500

    examples of let, var

---
+++ 05-behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,13 @@
+let name = "Umesh";
+
+if (name === "Umesh") {
+  let hobbies = ["tennis", "food"];
+}
+
+function greet() {
+  let age = 30;
+  console.log(name);
+}
+
+console.log(name, hobbies);
+greet();
