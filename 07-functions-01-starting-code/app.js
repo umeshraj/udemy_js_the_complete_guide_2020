193504140abba5c6549312fb821aa7dbfb5a289e
Umesh <umesh.rajashekar@gmail.com>
2019-12-06 06:30:29 -0500

    Validate user input

--- 07-functions-01-starting-code/app.js
+++ 07-functions-01-starting-code/app.js
@@ -1,5 +1,25 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+const getPlayerChoice = function() {
+  let selection = prompt(
+    `${ROCK}, ${PAPER}, or ${SCISSORS}?`,
+    ""
+  ).toUpperCase();
+
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    selection = DEFAULT_USER_CHOICE;
+    alert(`Invalid choice. We chose ${DEFAULT_USER_CHOICE} for you`);
+  }
+  return selection;
+};
+
 startGameBtn.addEventListener("click", function() {
   console.log("Game is starting..");
+  const playerChoice = getPlayerChoice();
+  console.log(playerChoice);
 });
