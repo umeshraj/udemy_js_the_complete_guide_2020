e25a979b5741e912060fa52742f02b5bf4aa7c0a
Umesh <umesh.rajashekar@gmail.com>
2019-12-06 05:58:08 -0500

    Function direct, indirect, method

--- 07-functions-01-starting-code/app.js
+++ 07-functions-01-starting-code/app.js
@@ -1,1 +1,20 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+
+function startGame() {
+  console.log("Game is starting..");
+}
+
+// direct call
+// startGame();
+
+// indirect call
+startGameBtn.addEventListener("click", startGame);
+
+// // objects can have functions (aka methods)
+// const person = {
+//   name: "Umesh",
+//   greet: function greet() {
+//     console.log("Hello, there");
+//   }
+// };
+// person.greet();
