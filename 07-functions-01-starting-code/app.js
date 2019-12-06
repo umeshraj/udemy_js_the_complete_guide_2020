6e16ab99b73234b61c6bd0636cbbbf97e04882ca
Umesh <umesh.rajashekar@gmail.com>
2019-12-06 06:09:25 -0500

    Function as an expression

--- 07-functions-01-starting-code/app.js
+++ 07-functions-01-starting-code/app.js
@@ -1,14 +1,14 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
-function startGame() {
+const start = function startGame() {
   console.log("Game is starting..");
-}
+};
 
 // direct call
 // startGame();
 
 // indirect call
-startGameBtn.addEventListener("click", startGame);
+startGameBtn.addEventListener("click", start);
 
 // // objects can have functions (aka methods)
 // const person = {
