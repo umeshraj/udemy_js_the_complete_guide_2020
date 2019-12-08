11ded58db79581cc9f21704e1c3bd505b7e87027
Umesh <umesh.rajashekar@gmail.com>
2019-12-08 10:56:08 -0500

    Function inside a function

--- 07-functions-01-starting-code/app.js
+++ 07-functions-01-starting-code/app.js
@@ -68,9 +68,13 @@ startGameBtn.addEventListener("click", () => {
 // not related to game
 
 const sumUp = (...numbers) => {
+  const validateNumber = number => {
+    return isNaN(number) ? 0 : number;
+  };
+
   let sum = 0;
   for (const num of numbers) {
-    sum += num;
+    sum += validateNumber(num);
   }
   return sum;
 };
