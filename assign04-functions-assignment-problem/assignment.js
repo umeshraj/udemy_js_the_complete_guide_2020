f8a21203d609aa82b5b4a7d520df1b442eea2a3e
Umesh <umesh.rajashekar@gmail.com>
2019-12-08 11:14:53 -0500

    part 2

--- assign04-functions-assignment-problem/assignment.js
+++ assign04-functions-assignment-problem/assignment.js
@@ -1,5 +1,19 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
-}
+// // part 1
+// const sayHello = name => {
+//   console.log("Hi " + name);
+// };
 
-sayHello();
+// // part 2a{
+// const sayHello = (greeting, name) => {
+//   console.log(`${greeting} ${name}`);
+// };
+
+// // part 2a{
+// const sayHello = () => {
+//   console.log("Hello, world!");
+// };
+
+// part 2c
+const sayHello = name => `Hello ${name}`;
+
+console.log(sayHello("Umesh"));
