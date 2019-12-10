6ddd6af48a6255e1d0c233f4a3db975513ad3ddc
Umesh <umesh.rajashekar@gmail.com>
2019-12-10 07:06:01 -0500

    Assignment solved

---
+++ 07-dom-assignment-start/assignment.js
@@ -0,0 +1,18 @@
+// part 1
+let task1 = document.querySelector("#task-1");
+task1.style.backgroundColor = "black";
+
+task1 = document.getElementById("task-1");
+task1.style.color = "white";
+
+// part 2
+let docTitle = document.querySelector("title");
+docTitle.text = "Assignment (solved)";
+
+const docHead = document.head;
+const docTitle2 = document.head.querySelector("title");
+docTitle2.textContent = "Assignment (solved 2)";
+
+// part 3
+const h1 = document.querySelector("h1");
+h1.textContent = "Assignment (solved)";
