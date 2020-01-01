52a3df5234470bbbcbc57ba896ea4cd781dcc50c
Umesh <umesh.rajashekar@gmail.com>
2020-01-01 06:42:49 -0500

    Render new movie on screen

--- 07-dom-06-demo-project-setup/assets/scripts/app.js
+++ 07-dom-06-demo-project-setup/assets/scripts/app.js
@@ -4,8 +4,36 @@ const backdrop = document.getElementById("backdrop");
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = document.querySelectorAll("input");
+const entryTextSection = document.getElementById("entry-text");
 
 const movies = [];
+
+const updateUI = () => {
+  // hide or show entry text
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  } else {
+    entryTextSection.style.display = "none";
+  }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement("li");
+  newMovieElement.className = "movie-element";
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+    <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+    <h2>${title}</h2>
+    <p>${rating}/5 stars</p>
+    </div>
+  `;
+
+  // append to list
+  const listRoot = document.getElementById("movie-list");
+  listRoot.append(newMovieElement);
+};
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -58,6 +86,8 @@ const addMovieHandler = () => {
   toggleMovieModal();
   // clear inputs
   clearMovieInput();
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  updateUI();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
