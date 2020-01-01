14a6c6636d525ae059e5a982b5c2af686daffce1
Umesh <umesh.rajashekar@gmail.com>
2019-12-31 19:34:59 -0500

    Toggle backdrop

--- 07-dom-06-demo-project-setup/assets/scripts/app.js
+++ 07-dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,8 +1,14 @@
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
+const backdrop = document.getElementById("backdrop");
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
 
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
