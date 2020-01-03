b2aa5999379c836088d6ea74cf330a21e9deeac4
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-03 07:08:35 -0500

    Using Array.from()

--- 08-arrays-01-starting-code/app.js
+++ 08-arrays-01-starting-code/app.js
@@ -8,3 +8,14 @@ console.log(moreNumbers);
 // another way
 const yetMoreNumbers = Array.of(1, 2);
 console.log(yetMoreNumbers);
+
+// convert iterable to array
+const iterArray = Array.from("Umesh");
+console.log(iterArray);
+
+// convert nodelist to array
+const listItems = document.querySelectorAll("li");
+console.log(listItems);
+
+const arrayListItems = Array.from(listItems);
+console.log(arrayListItems);
