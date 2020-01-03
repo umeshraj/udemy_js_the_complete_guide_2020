4c4d73ed74eb66619545b3b1438537ba0a0fbdc7
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-03 07:03:13 -0500

    Various ways of making arrays

---
+++ 08-arrays-01-starting-code/app.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+// using constructor
+const moreNumbers = new Array();
+console.log(moreNumbers);
+
+// another way
+const yetMoreNumbers = Array.of(1, 2);
+console.log(yetMoreNumbers);
