593aef58ee944313ed5a32aabcb4f859630c9018
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-04 07:13:02 -0500

    forEach()

--- 08-arrays-01-starting-code/app.js
+++ 08-arrays-01-starting-code/app.js
@@ -44,41 +44,57 @@
 // hobbies.splice(0, 1);
 // console.log(hobbies);
 
-// other array methods
-const testResults = [1, 1.3, 5, 8, 1.3, 9, -4];
-const storedResults = testResults.slice();
+// // other array methods
+// const testResults = [1, 1.3, 5, 8, 1.3, 9, -4];
+// const storedResults = testResults.slice();
 
-testResults.push(91);
-console.log(testResults, storedResults);
+// testResults.push(91);
+// console.log(testResults, storedResults);
 
-const partResults = testResults.slice(0, 3);
-console.log(partResults);
+// const partResults = testResults.slice(0, 3);
+// console.log(partResults);
 
-const concatResults = testResults.concat([1, 2]);
-console.log(concatResults);
+// const concatResults = testResults.concat([1, 2]);
+// console.log(concatResults);
 
-// index of
-console.log(testResults.indexOf(1.3));
-console.log(testResults.lastIndexOf(1.3));
+// // index of
+// console.log(testResults.indexOf(1.3));
+// console.log(testResults.lastIndexOf(1.3));
 
-// indexOf does not work for objects
-personData = [{ name: "Max" }, { name: "umesh" }];
-console.log(personData.indexOf({ name: "max" }));
+// // indexOf does not work for objects
+// personData = [{ name: "Max" }, { name: "umesh" }];
+// console.log(personData.indexOf({ name: "max" }));
 
-// finding objects in list
-const umesh = personData.find((person, idx, personData) => {
-  return person.name === "umesh";
+// // finding objects in list
+// const umesh = personData.find((person, idx, personData) => {
+//   return person.name === "umesh";
+// });
+// console.log(umesh);
+
+// umesh.name = "Anna";
+// console.log(personData);
+
+// // findIndex()
+// const maxIndex = personData.findIndex((person, index, personData) => {
+//   return person.name === "Max";
+// });
+// console.log(maxIndex, personData[maxIndex]);
+
+// // includes (primitive only)
+// console.log(testResults.includes(1.3));
+
+const prices = [10.99, 5.99, 4.9, 1.0];
+const tax = 0.19;
+const taxAdjustedPrices = [];
+
+// old for loop
+// for (const price of prices) {
+//   taxAdjustedPrices.push(price * (1 + tax));
+// }
+
+// for each
+prices.forEach((price, index, prices) => {
+  const priceObj = { index: index, taxAdjPrice: price * (1 + tax) };
+  taxAdjustedPrices.push(priceObj);
 });
-console.log(umesh);
-
-umesh.name = "Anna";
-console.log(personData);
-
-// findIndex()
-const maxIndex = personData.findIndex((person, index, personData) => {
-  return person.name === "Max";
-});
-console.log(maxIndex, personData[maxIndex]);
-
-// includes (primitive only)
-console.log(testResults.includes(1.3));
+console.log(taxAdjustedPrices);
