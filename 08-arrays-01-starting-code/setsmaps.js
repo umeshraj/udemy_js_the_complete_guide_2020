f3685de8b05809aaa6365b254e8f5d7191ec94e8
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-04 19:30:38 -0500

    maps()

--- 08-arrays-01-starting-code/setsmaps.js
+++ 08-arrays-01-starting-code/setsmaps.js
@@ -14,3 +14,38 @@ for (const entry of ids.entries()) {
 ids.add(2);
 ids.delete("Hi");
 console.log(ids);
+
+// MAPS
+const person1 = { name: "Umesh" };
+const person2 = { name: "Raj" };
+
+const personData = new Map([[person1, [{ date: "yesterday", price: 10 }]]]);
+console.log(personData);
+
+console.log(personData.get(person1));
+
+// add a key/value pair
+personData.set(person2, [{ date: "two weeks ago", price: 100 }]);
+console.log(personData);
+
+// show all key/value pairs
+for (const entry of personData.entries()) {
+  console.log(entry);
+}
+
+// array destructuring with maps
+for (const [key, value] of personData.entries()) {
+  console.log(key, value);
+}
+
+// just see the keys
+console.log("KEYS ONLY");
+for (const key of personData.keys()) {
+  console.log(key);
+}
+
+// just see the values
+console.log("Just values");
+for (const values of personData.values()) {
+  console.log(values);
+}
