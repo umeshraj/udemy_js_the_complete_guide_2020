12242991c96f4f1df64771d13f311dcaf5cfeef5
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-06 19:19:38 -0500

    Adding search options

--- 09-objects-01-starting-project/assets/scripts/objects.js
+++ 09-objects-01-starting-project/assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchMovieBtn = document.getElementById("search-btn");
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = "") => {
   const movieList = document.getElementById("movie-list");
   if (movies.length === 0) {
     movieList.classList.remove("visible");
@@ -12,7 +12,12 @@ const renderMovies = () => {
   }
   movieList.innerHTML = ""; // clear all elements (not ideal)
 
-  movies.forEach(movie => {
+  // get filtered movie list
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter(movie => movie.info.title.includes(filter));
+
+  filteredMovies.forEach(movie => {
     const movieEl = document.createElement("li");
     let text = movie.info.title + " - ";
     for (const key in movie.info) {
@@ -54,4 +59,10 @@ const addMovieHandler = () => {
   renderMovies();
 };
 
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById("filter-title").value;
+  renderMovies(filterTerm);
+};
+
 addMovieBtn.addEventListener("click", addMovieHandler);
+searchMovieBtn.addEventListener("click", searchMovieHandler);
