dad4f45ed297303788d933bf038de28f046b929f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-07 06:40:20 -0500

    Object destructuring

--- 09-objects-01-starting-project/assets/scripts/objects.js
+++ 09-objects-01-starting-project/assets/scripts/objects.js
@@ -19,10 +19,11 @@ const renderMovies = (filter = "") => {
 
   filteredMovies.forEach(movie => {
     const movieEl = document.createElement("li");
-    let text = movie.info.title + " - ";
-    for (const key in movie.info) {
+    const { info } = movie;
+    let text = info.title + " - ";
+    for (const key in info) {
       if (key !== "title") {
-        text = text + `${key}: ${movie.info[key]}`;
+        text = text + `${key}: ${info[key]}`;
       }
     }
     movieEl.textContent = text;
