eb961ba3942ca60e0e16584cbafec789cefc0445
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-06 19:10:30 -0500

    Use for-in loop to get user-entered key

--- 09-objects-01-starting-project/assets/scripts/objects.js
+++ 09-objects-01-starting-project/assets/scripts/objects.js
@@ -14,7 +14,14 @@ const renderMovies = () => {
 
   movies.forEach(movie => {
     const movieEl = document.createElement("li");
-    movieEl.textContent = movie.info.title;
+    let text = movie.info.title + " - ";
+    for (const key in movie.info) {
+      if (key !== "title") {
+        text = text + `${key}: ${movie.info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
+
     movieList.append(movieEl);
   });
 };
