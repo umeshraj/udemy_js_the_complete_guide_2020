c37e3b3b4d6603b963d135063adf187a433e706f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-08 06:34:28 -0500

    this inside an arrow fn refers to the window object

--- 09-objects-01-starting-project/assets/scripts/objects.js
+++ 09-objects-01-starting-project/assets/scripts/objects.js
@@ -68,7 +68,7 @@ const addMovieHandler = () => {
   renderMovies();
 };
 
-const searchMovieHandler = function() {
+const searchMovieHandler = () => {
   console.log(this);
   const filterTerm = document.getElementById("filter-title").value;
   renderMovies(filterTerm);
