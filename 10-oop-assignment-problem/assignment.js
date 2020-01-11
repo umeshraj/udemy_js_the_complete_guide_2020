f271f80ea8993366ef6ba7c6078a28a5e7d6da06
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-11 10:48:03 -0500

    part 3 inheritance

--- 10-oop-assignment-problem/assignment.js
+++ 10-oop-assignment-problem/assignment.js
@@ -29,3 +29,24 @@ console.log(course1.summary());
 console.log(course2.summary());
 
 // part 3
+
+class PracticalCourse extends Course {
+  constructor() {
+    super();
+    this.numOfExercises = 10;
+  }
+}
+
+class TheoreticalCourse extends Course {
+  constructor() {
+    super();
+  }
+
+  publish() {
+    console.log("Something..");
+  }
+}
+
+const pract = new PracticalCourse("ur", 10, 11);
+const theory = new TheoreticalCourse("ab", 1, 2);
+theory.publish();
