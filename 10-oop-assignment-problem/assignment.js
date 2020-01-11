17b4668de83a195b0ab938b9cd4e9572436261b9
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-11 10:40:41 -0500

    Add methods to class

--- 10-oop-assignment-problem/assignment.js
+++ 10-oop-assignment-problem/assignment.js
@@ -5,10 +5,27 @@ class Course {
     this.length = length;
     this.price = price;
   }
+
+  lengthToPrice() {
+    return this.length / this.price;
+  }
+
+  summary() {
+    const out = `Course: ${this.title}. Length: ${this.length}. Price: ${
+      this.price
+    } L2P: ${this.lengthToPrice().toFixed(2)}
+    `;
+    return out;
+  }
 }
 
 const course1 = new Course("JS Mc", 50, 10);
 const course2 = new Course("D3", 12, 89);
 
-console.log(course1);
-console.log(course2);
+// console.log(course1);
+// console.log(course2);
+
+console.log(course1.summary());
+console.log(course2.summary());
+
+// part 3
