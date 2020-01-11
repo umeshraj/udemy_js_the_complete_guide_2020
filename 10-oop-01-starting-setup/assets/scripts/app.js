3f2022a2941af6ce2bb7f67c62028d57698eb92c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-11 07:35:19 -0500

    Set arrow fn as a property to avoid problems with this

--- 10-oop-01-starting-setup/assets/scripts/app.js
+++ 10-oop-01-starting-setup/assets/scripts/app.js
@@ -65,18 +65,18 @@ class ShoppingCart extends Component {
   }
 
   constructor(renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
+    this.orderProducts = () => {
+      console.log("Ordering...");
+      console.log(this.items);
+    };
+    this.render();
   }
 
   addProduct(product) {
     const updatedItems = [...this.items]; // true copy of items
     updatedItems.push(product);
     this.cartItems = updatedItems;
-  }
-
-  orderProducts() {
-    console.log("Ordering...");
-    console.log(this.items);
   }
 
   render() {
@@ -87,7 +87,8 @@ class ShoppingCart extends Component {
       <button> Order now</button>
     `;
     const orderButton = cartEl.querySelector("button");
-    orderButton.addEventListener("click", () => this.orderProducts());
+    // orderButton.addEventListener("click", () => this.orderProducts());
+    orderButton.addEventListener("click", this.orderProducts);
     this.totalOutput = cartEl.querySelector("h2");
   }
 }
