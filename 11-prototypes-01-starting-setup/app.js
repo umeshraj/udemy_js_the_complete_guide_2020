b9e4516ce65530d6cf5c9b0a8b529e7927ba3003
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-12 11:42:21 -0500

    Extends & prototype

--- 11-prototypes-01-starting-setup/app.js
+++ 11-prototypes-01-starting-setup/app.js
@@ -1,37 +1,50 @@
 // Modern class
-// class Person {
-//   name = "Umesh";
 
-//   constructor() {
-//     this.age = 40;
-//   }
-
-//   greet() {
-//     console.log(`Hi. I am ${this.name}. I am ${this.age} years old`);
-//   }
-// }
-
-// Old way: constructor functions
-function Person() {
-  this.age = 40;
-  this.name = "Max";
-  this.greet = function() {
-    console.log(`Hi. I am ${this.name}. I am ${this.age} years old`);
-  };
-}
-
-Person.prototype = {
+class AgedPerson {
   printAge() {
     console.log(this.age);
   }
-};
+}
 
-const p = new Person();
-p.greet();
+class Person extends AgedPerson {
+  name = "Umesh";
 
-console.log(p.toString());
+  constructor() {
+    super();
+    this.age = 40;
+  }
 
-console.log(p.__proto__);
+  greet() {
+    console.log(`Hi. I am ${this.name}. I am ${this.age} years old`);
+  }
+}
+
+// // Old way: constructor functions
+// function Person() {
+//   this.age = 40;
+//   this.name = "Max";
+//   this.greet = function() {
+//     console.log(`Hi. I am ${this.name}. I am ${this.age} years old`);
+//   };
+// }
+
+// Person.prototype = {
+//   printAge() {
+//     console.log(this.age);
+//   }
+// };
+
+// // don't overwrite prototype
+// Person.prototype.printAge = function() {
+//   console.log(this.age);
+// };
+
+// console.log(p.toString());
+
+// console.log(p.__proto__);
 
 // using Person.prototype in an object
+const p = new Person();
+p.greet();
 p.printAge();
+console.log(p.__proto__);
