e3bcb604132cc3e89b3067589c27e901a3d0546f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-14 06:45:39 -0500

    Adding logic to remove item from one list and adding to other

--- 12-practice-oop-01-starting-setup/assets/scripts/app.js
+++ 12-practice-oop-01-starting-setup/assets/scripts/app.js
@@ -1,11 +1,38 @@
 class ToolTip {}
 
-class ProjectItem {}
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
+
+  connectMoreInfoButton() {}
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector("button:last-of-type");
+    // switchBtn.addEventListener("click", );
+  }
+}
 
 class ProjectList {
+  projects = [];
   constructor(type) {
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
-    console.log(prjItems);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject() {}
+
+  switchProject(projectId) {
+    // // long way to remove item
+    // const projectIndex = this.projects.findIndex(p => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+    // shorter way to remove item from list
+    this.projects = this.projects.filter(p => p.id !== projectId);
   }
 }
 
