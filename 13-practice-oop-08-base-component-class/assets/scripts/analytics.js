b80f49068bc1ef2f25d0fb7ac9bef7211175890e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-16 08:34:44 -0500

    clearTimeout()

--- 13-practice-oop-08-base-component-class/assets/scripts/analytics.js
+++ 13-practice-oop-08-base-component-class/assets/scripts/analytics.js
@@ -1,3 +1,7 @@
-setInterval(() => {
+const intervalId = setInterval(() => {
   console.log("Sending analytics data...");
 }, 3000);
+
+document
+  .getElementById("stop-analytics-btn")
+  .addEventListener("click", () => clearTimeout(intervalId));
