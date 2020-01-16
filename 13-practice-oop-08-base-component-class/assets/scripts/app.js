be5ad6921c8778d5eb723962a73b3eac545aea6d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-16 06:38:08 -0500

    Scrolling element into view

--- 13-practice-oop-08-base-component-class/assets/scripts/app.js
+++ 13-practice-oop-08-base-component-class/assets/scripts/app.js
@@ -9,6 +9,7 @@ class DOMHelper {
     const element = document.getElementById(elementId);
     const destinationElement = document.querySelector(newDestinationSelector);
     destinationElement.append(element);
+    element.scrollIntoView({ behavior: "smooth" });
   }
 }
 
