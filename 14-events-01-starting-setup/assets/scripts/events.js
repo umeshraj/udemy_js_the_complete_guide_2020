23e5e6e012cc3b84822996466e0393e13b3ca2cf
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-17 07:12:39 -0500

    Two other bad ways of adding event listeners

---
+++ 14-events-01-starting-setup/assets/scripts/events.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("button");
+
+// // another bad way
+// button.onclick = function() {
+//   alert("Hello, world!");
+// };
+
+// // another bad way
+// const buttonClickHandler = () => {
+//   alert("Hello, world!");
+// };
+// button.onclick = buttonClickHandler;
