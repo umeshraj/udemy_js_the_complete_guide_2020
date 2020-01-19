7318f37067f4fb4d9cde08be0b44726c78c4f498
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-19 10:41:44 -0500

    Example of infinite scrolling

--- 14-events-01-starting-setup/assets/scripts/events.js
+++ 14-events-01-starting-setup/assets/scripts/events.js
@@ -31,3 +31,19 @@ buttons.forEach(button => {
 window.addEventListener("scroll", event => {
   console.log(event);
 });
+
+// infinite scrolling
+let curElementNumber = 0;
+
+function scrollHandler() {
+  const distanceToBottom = document.body.getBoundingClientRect().bottom;
+
+  if (distanceToBottom < document.documentElement.clientHeight + 150) {
+    const newDataElement = document.createElement("div");
+    curElementNumber++;
+    newDataElement.innerHTML = `<p>Element ${curElementNumber}</p>`;
+    document.body.append(newDataElement);
+  }
+}
+
+window.addEventListener("scroll", scrollHandler);
