18cb42323a02cc199d3481eeaad4fa87f12f521c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-19 11:03:33 -0500

    event.stopPropagation()

--- 14-events-01-starting-setup/assets/scripts/events.js
+++ 14-events-01-starting-setup/assets/scripts/events.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll("button");
+const button = document.querySelector("button");
 
 // // another bad way
 // button.onclick = function() {
@@ -52,5 +52,20 @@ const anotherButtonClickHandler = () => {
 const form = document.querySelector("form");
 form.addEventListener("submit", event => {
   event.preventDefault();
+  console.log(event);
+});
+
+// Event propagation
+
+const div = document.querySelector("div");
+div.addEventListener("click", event => {
+  console.log("CLICKED DIV");
+  console.log(event);
+});
+
+button.addEventListener("click", event => {
+  event.stopPropagation();
+  event.stopImmediatePropagation();
+  console.log("CLICKED BUTTON");
   console.log(event);
 });
