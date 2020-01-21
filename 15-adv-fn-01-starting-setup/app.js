2dd8cedf1fe1a4b6e78fed761de14280228b19df
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-21 08:38:40 -0500

    Closures in practice

--- 15-adv-fn-01-starting-setup/app.js
+++ 15-adv-fn-01-starting-setup/app.js
@@ -43,14 +43,28 @@ function calculateTax(amount, tax) {
 const vatAmount = calculateTax(100, 0.19);
 const incomeTax = calculateTax(100, 0.25);
 
+let multiplier = 1.1;
+
 function createTaxCalculator(tax) {
   function calculateTax(amount) {
-    return amount * tax;
+    console.log(multiplier);
+    return amount * tax * multiplier;
   }
   return calculateTax;
 }
 
 const calculateVatAmount = createTaxCalculator(0.19);
 const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+multiplier = 1.2;
+
 console.log(calculateVatAmount(100));
 console.log(calculateVatAmount(200));
+
+// closure in practice
+let userName = "Umesh";
+function greetUser() {
+  console.log("Hi " + userName);
+}
+userName = "max";
+greetUser();
