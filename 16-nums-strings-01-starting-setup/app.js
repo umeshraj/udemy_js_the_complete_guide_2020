a6aed33757cd9f623ab11dffcac2c40291786e79
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-22 08:38:04 -0500

    Tagged templates

--- 16-nums-strings-01-starting-setup/app.js
+++ 16-nums-strings-01-starting-setup/app.js
@@ -3,3 +3,24 @@ function randomIntBetween(min, max) {
 }
 
 console.log(randomIntBetween(1, 6));
+
+// tagged literal strings
+
+function productDescription(strings, productName, productPrice) {
+  console.log(strings);
+  console.log(productName);
+  console.log(productPrice);
+  // changing based on inputs
+  let priceCategory = "cheap";
+  if (productPrice > 20) {
+    priceCategory = "fair";
+  }
+  //   return "This is a product";
+  return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`;
+}
+
+const prodName = "JS Course";
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}.`;
+console.log(productOutput);
