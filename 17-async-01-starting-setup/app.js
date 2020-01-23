a96b3f3ddb0c973a4a0a41d56553a6567517b033
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-23 06:52:06 -0500

    Example of code blocking other code execution

--- 17-async-01-starting-setup/app.js
+++ 17-async-01-starting-setup/app.js
@@ -1,8 +1,15 @@
-const button = document.querySelector('button');
-const output = document.querySelector('p');
+const button = document.querySelector("button");
+const output = document.querySelector("p");
 
 function trackUserHandler() {
-  console.log('Clicked!');
+  console.log("Clicked!");
 }
 
-button.addEventListener('click', trackUserHandler);
+button.addEventListener("click", trackUserHandler);
+
+// // Single threading in JS blocks code execution
+// let result = 0;
+// for (let i = 0; i < 100000000; i++) {
+//   result += i;
+// }
+// console.log(result);
