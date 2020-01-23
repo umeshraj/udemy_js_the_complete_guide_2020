bd98922f44b4ed7662f3d1ede2d2d056ff08f5fd
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-23 06:36:24 -0500

    Starter code for Module 17

---
+++ 17-async-01-starting-setup/app.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button');
+const output = document.querySelector('p');
+
+function trackUserHandler() {
+  console.log('Clicked!');
+}
+
+button.addEventListener('click', trackUserHandler);
