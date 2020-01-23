98aabc9e6fa974edf6baeed274f2089f189f1a4a
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-23 08:43:41 -0500

    Promisifying setTimeout()

--- 17-async-01-starting-setup/app.js
+++ 17-async-01-starting-setup/app.js
@@ -1,15 +1,25 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
+const setTimer = duration => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done");
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     posData => {
-      console.log(posData);
+      setTimer(2000).then(data => console.log(data, posData));
     },
     error => {
       console.log(error);
     }
   );
+  setTimer(2000).then(() => console.log("Timer done!"));
   console.log("Getting position data...");
 }
 
