09189a9acc2b0f3b09004c8ee9b042b7aa0a2942
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-24 08:50:33 -0500

    Populate post on screen

--- 18-http-01-starting-setup/assets/scripts/app.js
+++ 18-http-01-starting-setup/assets/scripts/app.js
@@ -1,3 +1,7 @@
+// add to posts
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+
 const xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
@@ -6,7 +10,13 @@ xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
 xhr.onload = function() {
   //   console.log(xhr.response);
   const listOfPosts = JSON.parse(xhr.response);
-  console.log(listOfPosts);
+  //   console.log(listOfPosts);
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    listElement.append(postEl);
+  }
 };
 
 xhr.send();
