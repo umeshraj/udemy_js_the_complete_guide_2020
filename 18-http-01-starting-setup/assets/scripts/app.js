1c77f749a61f8739e6b534dfc040c5ded42db9cb
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-25 14:46:49 -0500

    Then use try/catch in the async/await

--- 18-http-01-starting-setup/assets/scripts/app.js
+++ 18-http-01-starting-setup/assets/scripts/app.js
@@ -53,18 +53,22 @@ function sendHttpRequest(method, url, data) {
 // }
 
 async function fetchPosts() {
-  const responseData = await sendHttpRequest(
-    "GET",
-    "https://jsonplaceholder.typicode.com/pos"
-  );
-  const listOfPosts = responseData; // since we set responseType=json
+  try {
+    const responseData = await sendHttpRequest(
+      "GET",
+      "https://jsonplaceholder.typicode.com/pos"
+    );
+    const listOfPosts = responseData; // since we set responseType=json
 
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector("h2").textContent = post.title.toUpperCase();
-    postEl.querySelector("p").textContent = post.body;
-    postEl.querySelector("li").id = post.id;
-    listElement.append(postEl);
+    for (const post of listOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector("h2").textContent = post.title.toUpperCase();
+      postEl.querySelector("p").textContent = post.body;
+      postEl.querySelector("li").id = post.id;
+      listElement.append(postEl);
+    }
+  } catch (error) {
+    alert(error.message);
   }
 }
 
