7342c0a4e55acee09500f2ab7483e2ca868c0403
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-25 14:39:19 -0500

    Deleting items

--- 18-http-01-starting-setup/assets/scripts/app.js
+++ 18-http-01-starting-setup/assets/scripts/app.js
@@ -3,6 +3,7 @@ const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 const form = document.querySelector("#new-post form");
 const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -50,6 +51,7 @@ async function fetchPosts() {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
     postEl.querySelector("p").textContent = post.body;
+    postEl.querySelector("li").id = post.id;
     listElement.append(postEl);
   }
 }
@@ -74,4 +76,14 @@ form.addEventListener("submit", event => {
   const enteredTitle = event.currentTarget.querySelector("#title").value;
   const enteredConent = event.currentTarget.querySelector("#content").value;
   createPost(enteredTitle, enteredConent);
+});
+
+postList.addEventListener("click", event => {
+  if (event.target.tagName === "BUTTON") {
+    const postId = event.target.closest("li").id;
+    sendHttpRequest(
+      "DELETE",
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
 });
