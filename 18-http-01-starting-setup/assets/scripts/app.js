c9dda13d0338db2e0efbbeb1c5c3e957459468f2
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-26 11:51:41 -0500

    Error handling in fetch() is a bit complex

--- 18-http-01-starting-setup/assets/scripts/app.js
+++ 18-http-01-starting-setup/assets/scripts/app.js
@@ -62,16 +62,29 @@ function sendHttpRequest(method, url, data) {
     headers: {
       "Content-Type": "application/json"
     }
-  }).then(response => {
-    return response.json();
-  });
+  })
+    .then(response => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      } else {
+        // error
+        return response.json().then(errData => {
+          console.log(errData);
+          throw new Error("Something went wrong server side");
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      throw new Error("Something went wrong");
+    });
 }
 
 async function fetchPosts() {
   try {
     const responseData = await sendHttpRequest(
       "GET",
-      "https://jsonplaceholder.typicode.com/posts"
+      "https://jsonplaceholder.typicode.com/pos"
     );
     const listOfPosts = responseData; // since we set responseType=json
 
