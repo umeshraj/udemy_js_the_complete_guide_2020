bd35ee8f53c71217fe2a4f4a8d6367553512a2e3
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-27 08:29:10 -0500

    Exporting component

--- 20-modules-01-starting-project/assets/scripts/App/Component.js
+++ 20-modules-01-starting-project/assets/scripts/App/Component.js
@@ -1,4 +1,4 @@
-class Component {
+export class Component {
   constructor(hostElementId, insertBefore = false) {
     if (hostElementId) {
       this.hostElement = document.getElementById(hostElementId);
