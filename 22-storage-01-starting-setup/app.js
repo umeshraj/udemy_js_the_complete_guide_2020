d0c4498c43b7ddd6773a219f3a61c70f90fc4850
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-30 08:40:09 -0500

    Accessing cookie values

--- 22-storage-01-starting-setup/app.js
+++ 22-storage-01-starting-setup/app.js
@@ -9,5 +9,8 @@ storeBtn.addEventListener("click", () => {
 });
 
 retrBtn.addEventListener("click", () => {
-  console.log(document.cookie);
+  const cookieData = document.cookie.split(";");
+  const data = cookieData.map(i => i.trim());
+  //   console.log(data);
+  console.log(data[1].split("=")[1]);
 });
