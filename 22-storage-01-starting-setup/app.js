d1514510350a6938d8a9943922eb017af658bfe0
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-30 08:36:27 -0500

    Saving an object

--- 22-storage-01-starting-setup/app.js
+++ 22-storage-01-starting-setup/app.js
@@ -3,7 +3,9 @@ const retrBtn = document.getElementById("retrieve-btn");
 
 storeBtn.addEventListener("click", () => {
   const userId = "u123";
+  const user = { name: "Max", age: 30 };
   document.cookie = `uid=${userId}`;
+  document.cookie = `user=${JSON.stringify(user)}`;
 });
 
 retrBtn.addEventListener("click", () => {
