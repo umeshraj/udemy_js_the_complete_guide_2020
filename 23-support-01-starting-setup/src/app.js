45e90f919b736d6363f0742b8104d73d4152d893
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-31 07:21:13 -0500

    Feature detection

--- 23-support-01-starting-setup/src/app.js
+++ 23-support-01-starting-setup/src/app.js
@@ -3,12 +3,16 @@ const textParagaph = document.querySelector("p");
 
 button.addEventListener("click", () => {
   const text = textParagaph.textContent;
-  navigator.clipboard
-    .writeText(text)
-    .then(result => {
-      console.log(result);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  if (navigator.clipboard) {
+    navigator.clipboard
+      .writeText(text)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } else {
+    alert("Feature not available!");
+  }
 });
