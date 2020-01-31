57940f81cc2cfd3f356b65b2e1f7bab710e35d8d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-31 08:50:10 -0500

    Using smart include using babel

--- 23-support-01-starting-setup/src/app.js
+++ 23-support-01-starting-setup/src/app.js
@@ -1,10 +1,12 @@
-import "core-js";
+// import "core-js/features/promise";
 
 const button = document.querySelector("button");
 const textParagaph = document.querySelector("p");
 
 button.addEventListener("click", () => {
   const text = textParagaph.textContent;
+  const promise = new Promise();
+  console.log(promise);
   if (navigator.clipboard) {
     navigator.clipboard
       .writeText(text)
