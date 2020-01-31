606039c211422ca573e83b4cd16062deac09a76a
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-01-31 07:19:53 -0500

    Using navigator.cliipboard

--- 23-support-01-starting-setup/src/app.js
+++ 23-support-01-starting-setup/src/app.js
@@ -1,5 +1,14 @@
-const button = document.querySelector('button');
+const button = document.querySelector("button");
+const textParagaph = document.querySelector("p");
 
-button.addEventListener('click', () => {
-  // do something...
+button.addEventListener("click", () => {
+  const text = textParagaph.textContent;
+  navigator.clipboard
+    .writeText(text)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
