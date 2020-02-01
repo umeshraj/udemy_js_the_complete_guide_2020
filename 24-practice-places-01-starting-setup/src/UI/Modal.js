3f00754a780282c8033f233bb100b9814465790d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-02-01 10:22:24 -0500

    Hiding the modal

--- 24-practice-places-01-starting-setup/src/UI/Modal.js
+++ 24-practice-places-01-starting-setup/src/UI/Modal.js
@@ -11,21 +11,28 @@ export class Modal {
         this.modalTemplateEl.content,
         true
       );
-      const modalElement = modalElements.querySelector(".modal");
-      const backdropElement = modalElements.querySelector(".backdrop");
+      this.modalElement = modalElements.querySelector(".modal");
+      this.backdropElement = modalElements.querySelector(".backdrop");
       const contentElement = document.importNode(
         this.contentTemplateEl.content,
         true
       );
 
-      modalElement.appendChild(contentElement);
-      document.body.insertAdjacentElement("afterbegin", modalElement);
-      document.body.insertAdjacentElement("afterbegin", backdropElement);
+      this.modalElement.appendChild(contentElement);
+      document.body.insertAdjacentElement("afterbegin", this.modalElement);
+      document.body.insertAdjacentElement("afterbegin", this.backdropElement);
     } else {
       // fallback code
       alert(this.fallbackText);
     }
   }
 
-  hide() {}
+  hide() {
+    if (this.modalElement) {
+      document.body.removeChild(this.modalElement);
+      document.body.removeChild(this.backdropElement);
+      this.modalElement = null;
+      this.backdropElement = null;
+    }
+  }
 }
