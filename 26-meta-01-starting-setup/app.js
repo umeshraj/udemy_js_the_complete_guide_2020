b4d66a0c00bd191145962890cf159b8f4bfa3108
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-02-03 08:36:32 -0500

    Making our own iterator

--- 26-meta-01-starting-setup/app.js
+++ 26-meta-01-starting-setup/app.js
@@ -17,3 +17,34 @@ console.log(Symbol("uid") === Symbol("uid"));
 
 user[uid] = "p9";
 console.log(user.toString());
+
+const company = {
+  curEmployee: 0,
+  employees: ["Max", "Manu", "Anna"],
+  next() {
+    if (this.curEmployee >= this.employees.length) {
+      return { value: this.curEmployee, done: true };
+    }
+
+    const returnValue = {
+      value: this.employees[this.curEmployee],
+      done: false
+    };
+    this.curEmployee++;
+    return returnValue;
+  }
+};
+
+// // keep calling next
+// console.log(company.next());
+// console.log(company.next());
+// console.log(company.next());
+// console.log(company.next());
+// console.log(company.next());
+
+// calling next with a loop
+let employee = company.next();
+while (!employee.done) {
+  console.log(employee.value);
+  employee = company.next();
+}
