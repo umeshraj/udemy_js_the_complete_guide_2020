929a0447938e6ca8ba7ed95c0d7f2fa58258f86d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-02-04 08:58:41 -0500

    Form from node

--- 27-node-01-first-nodejs-code/app.js
+++ 27-node-01-first-nodejs-code/app.js
@@ -2,7 +2,9 @@ const http = require("http");
 
 const server = http.createServer((request, response) => {
   response.setHeader("Content-Type", "text/html");
-  response.write("<h1>Hello, there</h1>");
+  response.write(
+    "<form method='POST' action='/'><input name='username' type='text'><button type='submit'>Send</button></form>"
+  );
   response.end();
 });
 
