2e3b2a65f8e9b9f5957b9588cf3c83c1c7123bd4
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-02-04 08:44:53 -0500

    Reading from file

--- 27-node-01-first-nodejs-code/app.js
+++ 27-node-01-first-nodejs-code/app.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
 
+// reading from file
+fs.readFile("user-data.txt", (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log(data.toString());
+  }
+});
+
+// writing to file
 fs.writeFile("user-data.txt", "username=Umesh", err => {
   if (err) {
     console.log(err);
