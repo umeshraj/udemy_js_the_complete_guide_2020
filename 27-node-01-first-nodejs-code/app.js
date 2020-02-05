ef865566863fc855694d59ac26a9fca55c903fe6
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-02-05 06:33:54 -0500

    Body parser usage

--- 27-node-01-first-nodejs-code/app.js
+++ 27-node-01-first-nodejs-code/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+// body parsing with express
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader("Content-Type", "text/html");
@@ -8,7 +12,11 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.send("<h1>Hello, World!</h1>");
+  let userName = req.body.username || "Unknown user";
+  console.log(userName);
+  res.send(
+    `<h1>Hi ${userName}</h1><form method='POST' action='/'><input name='username' type='text'><button type='submit'>Send</button></form>`
+  );
 });
 
 app.listen(3000);
