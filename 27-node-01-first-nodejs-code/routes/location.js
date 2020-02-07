8f2012994328a8e91a792364c5197094710e3b2b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-02-07 06:23:23 -0500

    Using mongodb for storage

--- 27-node-01-first-nodejs-code/routes/location.js
+++ 27-node-01-first-nodejs-code/routes/location.js
@@ -1,30 +1,64 @@
 const express = require("express");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
 const router = express.Router();
+
+// Connection URL
+const url =
+  "mongodb+srv://<username>:<password>@cluster0-1woqp.mongodb.net/locations?retryWrites=true&w=majority";
+
+// Create a new MongoClient
+const client = new MongoClient(url);
 
 const locationStorage = {
   locations: []
 };
 
 router.post("/add-location", (req, res, next) => {
-  const id = Math.random();
-  locationStorage.locations.push({
-    id: id,
-    address: req.body.address,
-    coords: { lat: req.body.lat, lng: req.body.lng }
+  // const id = Math.random();
+  // Use connect method to connect to the Server
+  client.connect(function(err, client) {
+    const db = client.db("locations");
+
+    // Insert a single document
+    db.collection("user-locations").insertOne(
+      {
+        address: req.body.address,
+        coords: { lat: req.body.lat, lng: req.body.lng }
+      },
+      function(err, r) {
+        console.log(r);
+        res.json({ message: "Stored location!", locId: r.insertedId });
+      }
+    );
   });
-  res.json({ message: "Stored location!", locId: id });
+
+  // locationStorage.locations.push({
+  //   id: id,
+  //   address: req.body.address,
+  //   coords: { lat: req.body.lat, lng: req.body.lng }
+  // });
 });
 
 router.get("/location/:lid", (req, res, next) => {
-  const locationId = +req.params.lid;
-  const location = locationStorage.locations.find(loc => {
-    return loc.id === locationId;
+  const locationId = req.params.lid;
+  client.connect(function(err, client) {
+    const db = client.db("locations");
+
+    // Insert a single document
+    db.collection("user-locations").findOne(
+      {
+        _id: new mongodb.ObjectId(locationId)
+      },
+      function(err, doc) {
+        if (!doc) {
+          return res.status(404).json({ message: "Not found" });
+        }
+        res.json({ address: doc.address, coordinates: doc.coords });
+      }
+    );
   });
-  if (!location) {
-    return res.status(404).json({ message: "Not found" });
-  }
-  res.json({ address: location.address, coordinates: location.coords });
 });
 
 module.exports = router;
