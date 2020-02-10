a576f31b444b2db2976d5d2628dcf9598a755420
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-02-10 06:07:50 -0500

    Integration test

--- 31-testing-01-starting-setup/util.test.js
+++ 31-testing-01-starting-setup/util.test.js
@@ -1,4 +1,4 @@
-const { generateText } = require("./util");
+const { generateText, checkAndGenerate } = require("./util");
 
 test("should output name and age", () => {
   const text = generateText("Max", 40);
@@ -8,4 +8,9 @@ test("should output name and age", () => {
 test("should output data-less text", () => {
   const text = generateText("", null);
   expect(text).toBe(" (null years old)");
+});
+
+test("should generate a valid text output", () => {
+  const text = checkAndGenerate("Max", 29);
+  expect(text).toBe("Max (29 years old)");
 });
