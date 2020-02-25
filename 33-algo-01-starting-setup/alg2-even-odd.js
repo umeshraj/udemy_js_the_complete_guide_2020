047c703a02cd90cc56c47b816754e2626cb3f9d1
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-02-25 08:41:32 -0500

    Ternary operator for even odd

--- 33-algo-01-starting-setup/alg2-even-odd.js
+++ 33-algo-01-starting-setup/alg2-even-odd.js
@@ -1,10 +1,11 @@
 function isEvenOrOdd(number) {
-  const result = number % 2;
-  if (result == 0) {
-    return "Even";
-  } else {
-    return "Odd";
-  }
+  //   const result = number % 2;
+  //   if (result == 0) {
+  //     return "Even";
+  //   } else {
+  //     return "Odd";
+  //   }
+  return number % 2 ? "Odd" : "Even";
 }
 
 console.log(isEvenOrOdd(10));
