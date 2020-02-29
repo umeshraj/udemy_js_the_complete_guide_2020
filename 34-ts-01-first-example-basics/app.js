4ca28c99d19442826427d51f4642258e1844c282
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-02-29 11:37:23 -0500

    Using typescript to fix potential error

---
+++ 34-ts-01-first-example-basics/app.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+    return a + b;
+}
+var result = add(5, 3);
+console.log(result);
---
+++ 34-ts-01-first-example-basics/app.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+
+const result = add(5, 3);
+console.log(result);
