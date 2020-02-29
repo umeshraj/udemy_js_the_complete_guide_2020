dbc60c214da3a271b34ab62462c5c3a18b70c43c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-02-29 11:57:27 -0500

    Using type casting

--- 34-ts-01-first-example-basics/app.js
+++ 34-ts-01-first-example-basics/app.js
@@ -1,5 +1,17 @@
+var num1Input = document.getElementById("num1");
+var num2Input = document.getElementById("num2");
+var buttonElement = document.querySelector("button");
 function add(a, b) {
     return a + b;
 }
-var result = add(5, 3);
-console.log(result);
+function printResult(result) {
+    console.log(result);
+}
+// const result = add(5, 3);
+// console.log(result);
+buttonElement.addEventListener("click", function () {
+    var num1 = +num1Input.value;
+    var num2 = +num2Input.value;
+    var result = add(num1, num2);
+    printResult(result);
+});
--- 34-ts-01-first-example-basics/app.ts
+++ 34-ts-01-first-example-basics/app.ts
@@ -1,3 +1,7 @@
+const num1Input = document.getElementById("num1") as HTMLInputElement;
+const num2Input = document.getElementById("num2") as HTMLInputElement;
+const buttonElement = document.querySelector("button");
+
 function add(a: number, b: number) {
   return a + b;
 }
@@ -6,5 +10,12 @@ function printResult(result: any): void {
   console.log(result);
 }
 
-const result = add(5, 3);
-console.log(result);
+// const result = add(5, 3);
+// console.log(result);
+
+buttonElement.addEventListener("click", () => {
+  const num1 = +num1Input.value;
+  const num2 = +num2Input.value;
+  const result = add(num1, num2);
+  printResult(result);
+});
