066fb78a8e28c0cf8c19ac8f8c0112f79a026393
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-02 08:48:07 -0500

    Adding & removing tooltip in webcomponent

--- 35-web-components-tooltip/tooltip.js
+++ 35-web-components-tooltip/tooltip.js
@@ -1,12 +1,25 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
+    this._tooltipContainer;
   }
 
   connectedCallback() {
     const tooltipIcon = document.createElement("span");
     tooltipIcon.textContent = " (?)";
+    tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
+    tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
     this.appendChild(tooltipIcon);
+  }
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement("div");
+    this._tooltipContainer.textContent = "This is the tooltip text";
+    this.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.removeChild(this._tooltipContainer);
   }
 }
 
