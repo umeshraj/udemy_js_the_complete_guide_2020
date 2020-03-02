aaaec9b7c078a62fd219d766220f6eec2274dbd8
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-02 08:57:17 -0500

    Styling our custom element

--- 35-web-components-tooltip/tooltip.js
+++ 35-web-components-tooltip/tooltip.js
@@ -2,19 +2,28 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
+    this._tooltipText = "Dummy tooltip text";
   }
 
   connectedCallback() {
+    if (this.hasAttribute("text")) {
+      this._tooltipText = this.getAttribute("text");
+    }
     const tooltipIcon = document.createElement("span");
     tooltipIcon.textContent = " (?)";
     tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
     tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
     this.appendChild(tooltipIcon);
+    this.style.position = "relative";
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement("div");
-    this._tooltipContainer.textContent = "This is the tooltip text";
+    this._tooltipContainer.textContent = this._tooltipText;
+    this._tooltipContainer.style.backgroundColor = "black";
+    this._tooltipContainer.style.color = "white";
+    this._tooltipContainer.style.position = "absolute";
+    this._tooltipContainer.style.zIndex = 10;
     this.appendChild(this._tooltipContainer);
   }
 
