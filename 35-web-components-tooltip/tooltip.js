586500f22873d8ffe5e86440c55e20b0d758b1e3
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-03 06:12:35 -0500

    Shadow dom

--- 35-web-components-tooltip/tooltip.js
+++ 35-web-components-tooltip/tooltip.js
@@ -3,6 +3,7 @@ class Tooltip extends HTMLElement {
     super();
     this._tooltipContainer;
     this._tooltipText = "Dummy tooltip text";
+    this.attachShadow({ mode: "open" });
   }
 
   connectedCallback() {
@@ -13,7 +14,7 @@ class Tooltip extends HTMLElement {
     tooltipIcon.textContent = " (?)";
     tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
     tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
-    this.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(tooltipIcon);
     this.style.position = "relative";
   }
 
@@ -24,11 +25,11 @@ class Tooltip extends HTMLElement {
     this._tooltipContainer.style.color = "white";
     this._tooltipContainer.style.position = "absolute";
     this._tooltipContainer.style.zIndex = 10;
-    this.appendChild(this._tooltipContainer);
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
-    this.removeChild(this._tooltipContainer);
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 }
 
