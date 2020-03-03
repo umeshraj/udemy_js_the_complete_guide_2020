6d0ed5f594d66a6233064125f411980fa1462f5c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-03 06:19:35 -0500

    Using html templates for tooltip

--- 35-web-components-tooltip/tooltip.js
+++ 35-web-components-tooltip/tooltip.js
@@ -4,14 +4,15 @@ class Tooltip extends HTMLElement {
     this._tooltipContainer;
     this._tooltipText = "Dummy tooltip text";
     this.attachShadow({ mode: "open" });
+    const template = document.querySelector("#tooltip-template");
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   connectedCallback() {
     if (this.hasAttribute("text")) {
       this._tooltipText = this.getAttribute("text");
     }
-    const tooltipIcon = document.createElement("span");
-    tooltipIcon.textContent = " (?)";
+    const tooltipIcon = this.shadowRoot.querySelector("span");
     tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
     tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
     this.shadowRoot.appendChild(tooltipIcon);
