3e6c777e09620f8f9a34035f55e327341a4b4b7c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-04 06:34:37 -0500

    Assignment done

---
+++ 35-basics-assignment-start/info-toggle.js
@@ -0,0 +1,44 @@
+class InfoToggle extends HTMLElement {
+  constructor() {
+    super();
+    this._isVisible = false;
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+    <style>
+        #info-box {
+        display: none;
+        }
+    </style>
+
+    <button>Show</button>
+    <p id="info-box">
+        <slot></slot>
+    </p>
+
+    `;
+    this._toggleButton = this.shadowRoot.querySelector("button");
+    this._infoBox = this.shadowRoot.querySelector("#info-box");
+    this._toggleButton.addEventListener(
+      "click",
+      this._toggleInfoBox.bind(this)
+    );
+  }
+
+  connectedCallback() {
+    if (this.hasAttribute("is-visible")) {
+      if (this.getAttribute("is-visible") === "true") {
+        this._isVisible = true;
+        this._infoBox.style = "block";
+        this._toggleButton.textContent = "Hide";
+      }
+    }
+  }
+
+  _toggleInfoBox() {
+    this._isVisible = !this._isVisible;
+    this._infoBox.style.display = this._isVisible ? "block" : "none";
+    this._toggleButton.textContent = this._isVisible ? "Hide" : "Show";
+  }
+}
+
+customElements.define("uc-info-toggle", InfoToggle);
