49f1b61467f950d96ea9c009d1937c9ff60fc93d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-04 08:55:21 -0500

    Host context styling

--- 35-web-components-tooltip/tooltip.js
+++ 35-web-components-tooltip/tooltip.js
@@ -18,6 +18,10 @@ class Tooltip extends HTMLElement {
         background: #ccc;
       }
 
+      :host-context(p){
+        font-weight: bold;
+      }
+
       .highlight{
         background-color: red;
       }
