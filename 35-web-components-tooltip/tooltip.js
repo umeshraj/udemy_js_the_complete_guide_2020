ed1aad0fa04c1c14bb22ea15df434850b7a19734
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-04 06:08:37 -0500

    Using styles in js

--- 35-web-components-tooltip/tooltip.js
+++ 35-web-components-tooltip/tooltip.js
@@ -5,7 +5,17 @@ class Tooltip extends HTMLElement {
     this._tooltipText = "Dummy tooltip text";
     this.attachShadow({ mode: "open" });
     const template = document.querySelector("#tooltip-template");
-    this.shadowRoot.innerHTML = `<slot>Some default</slot><span> (?)</span>`;
+    this.shadowRoot.innerHTML = `
+    <style>
+      div {
+        background-color: black;
+        color: white;
+        position: absolute;
+        z-index: 10;
+      }
+    </style>
+    <slot>Some default</slot><span> (?)</span>
+    `;
   }
 
   connectedCallback() {
@@ -22,10 +32,6 @@ class Tooltip extends HTMLElement {
   _showTooltip() {
     this._tooltipContainer = document.createElement("div");
     this._tooltipContainer.textContent = this._tooltipText;
-    this._tooltipContainer.style.backgroundColor = "black";
-    this._tooltipContainer.style.color = "white";
-    this._tooltipContainer.style.position = "absolute";
-    this._tooltipContainer.style.zIndex = 10;
     this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
