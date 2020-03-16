a3a6bc97e7d45e56235015078b3161d87ab1178e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-16 06:25:50 -0400

    Listen to changes in attributes

--- 35-web-components-tooltip/tooltip.js
+++ 35-web-components-tooltip/tooltip.js
@@ -62,6 +62,14 @@ class Tooltip extends HTMLElement {
     this.style.position = "relative";
   }
 
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(oldValue, newValue);
+  }
+
+  static get observedAttributes() {
+    return ["text"];
+  }
+
   _showTooltip() {
     this._tooltipContainer = document.createElement("div");
     this._tooltipContainer.textContent = this._tooltipText;
