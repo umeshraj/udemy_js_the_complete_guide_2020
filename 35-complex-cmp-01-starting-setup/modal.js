6d489f0faab14744cacb87079cab8d8da07727cc
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-17 06:47:58 -0400

    Styling the modal

--- 35-complex-cmp-01-starting-setup/modal.js
+++ 35-complex-cmp-01-starting-setup/modal.js
@@ -11,6 +11,19 @@ class Modal extends HTMLElement {
                 width:100%;
                 height: 100vh;
                 background: rgba(0,0,0,0.75);
+                z-index; 10;
+            }
+
+            #modal{
+                position: fixed;
+                top: 15vh;
+                left: 25%;
+                width: 50%;
+                height: 30rem;
+                z-index: 100;
+                background: white;
+                border-radius: 3px;
+                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25)
             }
         </style>
         <div id="backdrop"></div>
