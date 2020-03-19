21be3e7fd498568d48b50c30cbc32a637c3cbf8f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-19 08:36:50 -0400

    Dispatching event from component

--- 35-complex-cmp-01-starting-setup/modal.js
+++ 35-complex-cmp-01-starting-setup/modal.js
@@ -89,6 +89,9 @@ class Modal extends HTMLElement {
 
     cancelButton.addEventListener("click", this._cancel.bind(this));
     confirmButton.addEventListener("click", this._confirm.bind(this));
+    cancelButton.addEventListener("cancel", () => {
+      console.log("Cancel from inside the component");
+    });
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
@@ -119,8 +122,10 @@ class Modal extends HTMLElement {
     this.isOpen = false;
   }
 
-  _cancel() {
+  _cancel(event) {
     this.hide();
+    const cancelEvent = new Event("cancel");
+    event.target.dispatchEvent(cancelEvent);
   }
 
   _confirm() {
