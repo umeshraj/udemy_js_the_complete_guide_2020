a10e4a7993bcc3ea7e7de0d7ad60b478a6d5d950
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-19 08:46:27 -0400

    Animating the modal

--- 35-complex-cmp-01-starting-setup/modal.js
+++ 35-complex-cmp-01-starting-setup/modal.js
@@ -24,9 +24,14 @@ class Modal extends HTMLElement {
                 pointer-events: all;
             }
 
+            :host([opened]) #modal
+            {
+                top:15vh;
+            }
+
             #modal{
                 position: fixed;
-                top: 15vh;
+                top: 10vh;
                 left: 25%;
                 width: 50%;
                 // height: 30rem;
@@ -39,14 +44,17 @@ class Modal extends HTMLElement {
                 justify-content: space-between;
                 opacity: 0;
                 pointer-events: none;
+                transition: all 0.3s ease-out;
             }
 
             header{
                 padding: 1rem;
+                border-bottom: 1px solid #ccc;
             }
 
             ::slotted(h1){
                 font-size: 1.25rem;
+                margin: 0;
             }
 
             #main{
@@ -84,9 +92,11 @@ class Modal extends HTMLElement {
       console.dir(slots[1].assignedNodes());
     });
 
+    const backdrop = this.shadowRoot.querySelector("#backdrop");
     const cancelButton = this.shadowRoot.querySelector("#cancel-btn");
     const confirmButton = this.shadowRoot.querySelector("#confirm-btn");
 
+    backdrop.addEventListener("click", this._cancel.bind(this));
     cancelButton.addEventListener("click", this._cancel.bind(this));
     confirmButton.addEventListener("click", this._confirm.bind(this));
     // cancelButton.addEventListener("cancel", () => {
